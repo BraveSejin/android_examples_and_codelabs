650149cdba9fa6414dd8a8da0296e8f12eb46ec8
BraveSejin <ihavebravebiz@gmail.com>
2022-02-13 12:41:04 +0900

    setting - navigation

--- AboutMeDataBinding-Starter/app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ AboutMeDataBinding-Starter/app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -24,6 +24,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.android.aboutme.databinding.ActivityMainBinding
 
 
 /**
@@ -35,25 +37,37 @@ import androidx.appcompat.app.AppCompatActivity
  */
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Aleks Haecky")
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
+
     }
 
     /**
      * Click handler for the Done button.
      */
     private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
 
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        binding.apply {
+            myName?.nickname = binding.nicknameEdit.text.toString()
+            nicknameEdit.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+            doneButton.visibility = View.GONE
+
+            invalidateAll()
+        }
 
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
