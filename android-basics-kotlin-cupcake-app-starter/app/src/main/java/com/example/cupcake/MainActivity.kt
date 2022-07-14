1d423f1508dbae6da7ddcf52511fbcf806c24e86
BraveSejin <ihavebravebiz@gmail.com>
2022-07-14 12:58:12 +0900

    cupcake: backstack - 1

--- android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/MainActivity.kt
+++ android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -18,6 +18,7 @@ package com.example.cupcake
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -26,15 +27,24 @@ import androidx.navigation.ui.setupActionBarWithNavController
  * Activity for cupcake order flow.
  * super.onCreate, setContentView 까지 한거랑 동일하다.
  */
-class MainActivity : AppCompatActivity(R.layout.activity_main){
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //navController를 이용해 앱바의 제목을 변경가능navhostfragment ㅁㄴㅇㄹ
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.findNavController()
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
 
         // 앱 바에 제목을 성정하고, 뒤로가기 표시를 넣어준다.
         setupActionBarWithNavController(navController)
+    }
+
+    // 재정의해야 up buntton이 생깁니다.
+    //앱에서 위로 이동을 처리하도록 navController에게 요청
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
