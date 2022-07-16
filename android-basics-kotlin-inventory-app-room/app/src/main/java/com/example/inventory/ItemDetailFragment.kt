90f156a022226af0bd8ef2f6b62462af8999bf23
BraveSejin <ihavebravebiz@gmail.com>
2022-07-16 22:31:22 +0900

    flow asLive -> observe

--- android-basics-kotlin-inventory-app-room/app/src/main/java/com/example/inventory/ItemDetailFragment.kt
+++ android-basics-kotlin-inventory-app-room/app/src/main/java/com/example/inventory/ItemDetailFragment.kt
@@ -21,8 +21,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.inventory.data.Item
 import com.example.inventory.databinding.FragmentItemDetailBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
@@ -34,6 +36,14 @@ class ItemDetailFragment : Fragment() {
 
     private var _binding: FragmentItemDetailBinding? = null
     private val binding get() = _binding!!
+
+    lateinit var item: Item
+
+    private val viewModel: InventoryViewModel by activityViewModels {
+        InventoryViewModelFactory(
+            (activity?.application as InventoryApplication).database.itemDao()
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -57,6 +67,24 @@ class ItemDetailFragment : Fragment() {
                 deleteItem()
             }
             .show()
+    }
+
+    private fun bind(item: Item) {
+        binding.apply {
+            itemName.text = item.itemName
+            itemPrice.text = item.itemPrice.toString()
+            itemCount.text = item.quantityInStock.toString()
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val id = navigationArgs.itemId
+        viewModel.retrieveItem(id).observe(this.viewLifecycleOwner) { selectedItem ->
+            item = selectedItem
+            bind(item)
+        }
     }
 
     /**
