ff756bd4ce7dafb2a7e98b0f1f82f954d88f213f
BraveSejin <ihavebravebiz@gmail.com>
2022-07-17 14:16:44 +0900

    init words_preference_datastore

---
+++ words_preference_datastore/app/src/main/java/com/example/wordsapp/data/SettingDataStore.kt
@@ -0,0 +1,7 @@
+package com.example.wordsapp.data
+
+import android.content.Context
+
+class SettingDataStore(context: Context) {
+//    private const val LAYOUT_PREFERENCE_NAME = "layout_preferences"
+}
