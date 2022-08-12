679522bd20c765f7ca7ac82b96bd8207d914b117
BraveSejin <ihavebravebiz@gmail.com>
2022-08-12 14:01:10 +0900

    livedata test

--- android-testing-starter_code/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ android-testing-starter_code/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,15 +1,41 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.Event
 import org.junit.Test
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.*
+import org.junit.Rule
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 internal class TasksViewModelTest {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+
     @Test
     fun addNewTask_setsNewTaskEvent() {
         val taskViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
         taskViewModel.addNewTask()
+
+        val value = taskViewModel.newTaskEvent.getOrAwaitValue()
+        assertThat(value.getContentIfNotHandled(), not(nullValue()))
+
+
+//        val observer = Observer<Event<Unit>> {}
+//        try {
+//            taskViewModel.newTaskEvent.observeForever(observer)
+//            taskViewModel.addNewTask()
+//            val value = taskViewModel.newTaskEvent.value
+//
+//            assertThat(value?.getContentIfNotHandled(), (not(nullValue())))
+//        } finally {
+//            taskViewModel.newTaskEvent.removeObserver(observer)
+//        }
     }
 }
