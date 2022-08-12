f66ec9b28b922c5fe1145e5684593f91fb955641
BraveSejin <ihavebravebiz@gmail.com>
2022-08-12 14:43:18 +0900

    livedata test

--- android-testing-starter_code/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ android-testing-starter_code/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -8,34 +8,47 @@ import com.example.android.architecture.blueprints.todoapp.Event
 import org.junit.Test
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 internal class TasksViewModelTest {
 
+    private lateinit var tasksViewModel: TasksViewModel
+
+    @Before
+    fun setupViewModel() {
+        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+    }
+
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
-
     @Test
     fun addNewTask_setsNewTaskEvent() {
-        val taskViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
-        taskViewModel.addNewTask()
+        tasksViewModel.addNewTask()
 
-        val value = taskViewModel.newTaskEvent.getOrAwaitValue()
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
         assertThat(value.getContentIfNotHandled(), not(nullValue()))
-
 
 //        val observer = Observer<Event<Unit>> {}
 //        try {
-//            taskViewModel.newTaskEvent.observeForever(observer)
-//            taskViewModel.addNewTask()
-//            val value = taskViewModel.newTaskEvent.value
+//            taskViewModesl.newTaskEvent.observeForever(observer)
+//            taskViewModesl.addNewTask()
+//            val value = taskViewModesl.newTaskEvent.value
 //
 //            assertThat(value?.getContentIfNotHandled(), (not(nullValue())))
 //        } finally {
-//            taskViewModel.newTaskEvent.removeObserver(observer)
+//            taskViewModesl.newTaskEvent.removeObserver(observer)
 //        }
+    }
+
+    @Test
+    fun setFilterAllTasks_tasksAddViewVisible() {
+
+        tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+
+        assertThat(tasksViewModel.tasksAddViewVisible.getOrAwaitValue(), `is`(true))
     }
 }
